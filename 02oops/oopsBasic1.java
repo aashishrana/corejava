115e60b03b8cfb184fd3faa29e6bca17b94031ea
aashishrana <aashishrana71135@gmail.com>
2024-03-20 12:23:19 +0530

    Inheritance in java

---
+++ 02oops/oopsBasic1.java
@@ -0,0 +1,20 @@
+public class oopsBasic1 {
+  int x = 5;
+
+  public static void main(String[] args) {
+    oopsBasic1 myObj = new oopsBasic1();
+    System.out.println(myObj.x);
+  }
+}
+
+ class Divya {
+  int x;
+
+  public static void main(String[] args) {
+    Main myObj = new Main();
+    myObj.x = 40;
+    System.out.println(myObj.x);
+  }
+}
+ 
+
