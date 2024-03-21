5bb74aa9ada7d1ad00f4085dc48d1dfa07eb5bf3
aashishrana <aashishrana71135@gmail.com>
2024-03-21 11:28:16 +0530

    operators in java

---
+++ 03operators/ArithOperator.java
@@ -0,0 +1,17 @@
+public class ArithOperator {
+    public static void main(String []args) {
+        int num_1 = 10; int num_2 = 20;
+
+        // System.out.println(num_1 + num_2);
+        // System.out.println(num_1 - num_2);
+        // // System.out.println(num_1 * num_2);
+        System.out.println(num_1 % num_2);
+        // System.out.println(num_1 / num_2);
+        // System.out.println(num_1 > num_2);
+        // System.out.println(num_1 < num_2);
+        // System.out.println(num_1 <= num_2);
+        // System.out.println(num_1 >= num_2);
+        // System.out.println(++num_1 + num_2++);
+        // System.out.println(num_1 + num_2);
+    }
+}
